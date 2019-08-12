9dfc4338664384ffcb6043fad863aa252c099eb5
Sarat <saratsarat7@gmail.com>
2019-08-12 23:03:50 +0530

    Find your hand and jokers

--- new.py
+++ new.py
@@ -1,9 +1,10 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+from operator import itemgetter
 
 # Test Image
-img_rgb = cv.imread('test.png')
+img_rgb = cv.imread('full.png')
 img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
 
 def image_max(image):
@@ -20,13 +21,16 @@ def image_min(image):
     image = cv.resize(image, dim, interpolation = cv.INTER_AREA)
     return image
 
-def find_value(name):
+def find_value(name, rotation):
     # Test Image
     global img_rgb
     global img_gray
 
     # Symbol
-    symbol_loc = "images/" + name + ".png"
+    if (rotation == 0):
+        symbol_loc = "images/" + name + ".png"
+    else:
+        symbol_loc = "joker/" + name + ".png"
     test_symbol = cv.imread(symbol_loc,0)
 
     # Shape
@@ -38,13 +42,58 @@ def find_value(name):
     # Accuracy Percentage
     threshold = 0.9
 
+    # Points List
+    points = []
+
     # Selectors
     loc1 = np.where( match_symbol >= threshold)
     for pt in zip(*loc1[::-1]):
-        cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-        print(pt)
+        color = list(np.random.choice(range(256), size=3))
+        # cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+        cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), color, 2)
+        points.append(pt)
 
-    cv.imshow('Result', img_rgb)
-    cv.waitKey(0)
+    return points
 
-find_value("K")
+def sort_order(list):
+    return sorted(list, key=itemgetter(3))
+
+def match_finder(list1, list2):
+    for item1 in list1:
+        for item2 in list2:
+            if ((abs(item1[0]-item2[0]) < 5)):
+                if (item2[1] > 350):
+                    return (0, item2)
+                else:
+                    return (1, item2)
+
+def card_finder():
+    card_list = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    cart_type = ["diamond", "club", "spade", "heart"]
+
+    your_hand = []
+    common = []
+    joker = []
+
+    for type in cart_type:
+        for item in card_list:
+            result = match_finder(find_value(type,0), find_value(item,0))
+            if (result is not None):
+                joker_list = find_value(item, 1)
+                if (len(joker) > 0):
+                    joker.append((type+item, result[1]))
+                
+                if (result[0] == 0):
+                    your_hand.append((type+item, result[1]))
+                else:
+                    common.append((type+item, result[1]))
+
+    print(your_hand)
+    print(common)
+    print(joker)
+
+card_finder()
+cv.namedWindow("Result", cv.WND_PROP_FULLSCREEN)
+cv.setWindowProperty("Result",cv.WND_PROP_FULLSCREEN,cv.WINDOW_FULLSCREEN)
+cv.imshow("Result", img_rgb)
+cv.waitKey(0)
